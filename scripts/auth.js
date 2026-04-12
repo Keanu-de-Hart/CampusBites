506cc337b0bb48df1ae4be48512896f313477814
Mpilo <mpilomastermind@gmail.com>
2026-04-12 13:48:15 +0200

    fix: vendor system, auth flow, Firebase Storage uploads, and Firestore rules for public browse access

--- scripts/auth.js
+++ scripts/auth.js
@@ -6,10 +6,15 @@ from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
 export function initAuthUI() {
     const CustomerdashboardLink = document.getElementById("CustomerdashboardLink");
     const VendordashboardLink = document.getElementById("VendordashboardLink");
+    const AdmindashboardLink = document.getElementById("AdmindashboardLink");
     const loginBtn = document.getElementById("loginLink");
     const logoutBtn = document.getElementById("logoutBtn");
 
   onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+    // optional safety redirect for protected pages
+    return;
+  }
     if (user) {
     const userRef = doc(db, "users", user.uid);
     const userSnap = await getDoc(userRef);
@@ -21,6 +26,8 @@ export function initAuthUI() {
           CustomerdashboardLink?.classList.remove("hidden");
         } else if(data.role === "vendor"){
           VendordashboardLink?.classList.remove("hidden");
+        } else if(data.role === "admin"){
+          AdmindashboardLink?.classList.remove("hidden");
         }
       logoutBtn?.classList.remove("hidden");
       loginBtn?.classList.add("hidden");
@@ -30,18 +37,23 @@ export function initAuthUI() {
       // ❌ User is NOT logged in
       CustomerdashboardLink?.classList.add("hidden");
       VendordashboardLink?.classList.add("hidden");
+      AdmindashboardLink?.classList.add("hidden");
       logoutBtn?.classList.add("hidden");
       loginBtn?.classList.remove("hidden");
       console.log("User is not logged in.");
     }
   });
 }
-export function logout() {
-  signOut(auth)
-    .then(() => {
-      console.log("User signed out successfully");
-    })
-    .catch((error) => {
-      console.error("Error signing out:", error);
-    });
+export async function logout() {
+  try {
+    await signOut(auth);
+
+    console.log("User signed out successfully");
+
+    // 🔥 ALWAYS redirect after logout
+    window.location.href = "index.html";
+
+  } catch (error) {
+    console.error("Error signing out:", error);
+  }
 }
